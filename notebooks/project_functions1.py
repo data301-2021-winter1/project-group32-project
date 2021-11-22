8a00f4f8e5e1e7073ad931cd665e559732c8fd7a
meganstanwood <mestanwood@gmail.com>
2021-11-21 18:15:39 -0800

    added cleandata2.csv to processed data folder

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -1,10 +1,13 @@
 import pandas as pd
 def load_and_process(filing_counts):
     df = (
-    pd.read_csv('filing_counts.csv')   
-    .drop('for_total',axis=1)
-    .drop('case_action',axis=1)
-    .assign(state_agegroup=lambda x: x['age_group']+x['state'])
-)
+        pd.read_csv('filing_counts.csv')   
+    )
+    df1 =(
+        df
+        .drop('for_total',axis=1)
+        .drop('case_action',axis=1)
+        .assign(state_agegroup=lambda x: x['age_group']+x['state'])
+    )
 
     return df
