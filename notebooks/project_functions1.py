8b49917086c2946a60eef3b7e761851c4274b919
HannahLudbrook <ludbrookh@gmail.com>
2021-11-26 13:03:23 -0700

    cleaning up analysis

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -1,24 +1,16 @@
 import pandas as pd
 import numpy as np
 
+#Method Chain 1
 def load_and_process(filing_counts):
     df = (
         pd.read_csv('filing_counts.csv')   
-<<<<<<< HEAD
         .drop('for_total',axis=1)
         .drop('case_action',axis=1)
         .drop('year', axis=1)
         .assign(state_agegroup=lambda x: x['age_group']+x['state'])
+        .drop_duplicates(inplace=True)
 )
     return df
-=======
-    )
-    df1 =(
-        df
-        .drop('for_total',axis=1)
-        .drop('case_action',axis=1)
-        .assign(state_agegroup=lambda x: x['age_group']+x['state'])
-    )
-
-    return df
->>>>>>> 934944162d38fda218199cad19bbb69c0112a4b1
+   
+#Method Chain 2
